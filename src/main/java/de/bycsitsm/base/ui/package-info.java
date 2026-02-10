4d29d7b02a49d7b3eaef2b650f38133b5a9f5986
start.vaadin.com <no-reply@vaadin.com>
2026-02-10 08:12:07 +0000

    Generated project

---
+++ src/main/java/de/bycsitsm/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package de.bycsitsm.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/de/bycsitsm/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package de.bycsitsm.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
